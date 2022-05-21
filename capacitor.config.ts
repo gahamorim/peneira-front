f59f7a150acfc4eb43837d6a4ed5ffaf87dac2aa
Gabriel Amorim <gaahamorim95@gmail.com>
2022-05-21 12:09:24 -0300

    chore: CRA + Eslint/Pretier + Capacitor w/ Android setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.peneira.app",
+  appName: "Peneira",
+  webDir: "build",
+  bundledWebRuntime: false,
+};
+
+export default config;
